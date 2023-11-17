1d6af9b4a2e7bf5574ebfae2a42c921876eebf6a
EldenZheng <elden.zheng@gmail.com>
2023-11-17 19:10:09 +0800

    bootstrap modal v2

--- client/src/EmployeeInformation.jsx
+++ client/src/EmployeeInformation.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 // import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope , faUser , faAddressCard , faBriefcase , faPhone , faLocationDot , faCalendar , faStar , faUsers , faIdCard } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope , faUser , faAddressCard , faBriefcase , faPhone , faLocationDot , faCalendar , faStar , faUsers } from '@fortawesome/free-solid-svg-icons'
 
 export default function EmployeeModal(props) {
 	return (
 	  <Modal
 		{...props}
-		size="lg"
-		aria-labelledby="contained-modal-title-vcenter"
+        size='lg'
+		dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
 		centered
 	  >
 		<Modal.Header closeButton>
 		  <Modal.Title id="contained-modal-title-vcenter">
-          <FontAwesomeIcon icon={faIdCard} /> Employee Information
+          <h1><FontAwesomeIcon icon={faUser} /> Employee Information</h1>
 		  </Modal.Title>
 		</Modal.Header>
-		<Modal.Body>
+		<Modal.Body >
             <div className='employeeDetContainer'>
                 <div>
                     <div className='imageContainer'>
@@ -28,15 +29,44 @@ export default function EmployeeModal(props) {
                     </div>
                 </div>
                 <div>
-                    <h4> <FontAwesomeIcon icon={faUser} /> {props.userdata.name}</h4>
-                    <FontAwesomeIcon icon={faEnvelope} /> <label>{props.userdata.email}</label><br/>
-                    <FontAwesomeIcon icon={faAddressCard} /> <label>{props.userdata.ic}</label><br/>
-                    <FontAwesomeIcon icon={faBriefcase} /> <label>{props.userdata.dept} - {props.userdata.role}</label><br/>
-                    <FontAwesomeIcon icon={faPhone} /> <label>{props.userdata.phone}</label><br/>
-                    <FontAwesomeIcon icon={faLocationDot} /> <label>{props.userdata.address}</label><br/>
-                    <FontAwesomeIcon icon={faCalendar} /> <label>{props.userdata.hiredDate}</label><br/>
-                    <FontAwesomeIcon icon={faStar} /> <label>{props.userdata.rating}</label><br/>
-                    <FontAwesomeIcon icon={faUsers} /> <label>Team {props.userdata.team}</label><br/>
+                    <table>
+                        <tr style={{borderBottom: '1px solid gray'}}>
+                            {/* <td><h2><FontAwesomeIcon icon={faUser} /></h2></td> */}
+                            <td><h2>{props.userdata.name}</h2></td>
+                        </tr>
+                        <tr style={{marginTop:'10px'}}>
+                            <td><FontAwesomeIcon icon={faEnvelope} /></td>
+                            <td><label>{props.userdata.email}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faAddressCard} /></td>
+                            <td><label>{props.userdata.ic}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faBriefcase} /></td>
+                            <td><label>{props.userdata.dept} - {props.userdata.role}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faPhone} /></td>
+                            <td><label>{props.userdata.phone}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faLocationDot} /></td>
+                            <td><label>{props.userdata.address}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faCalendar} /></td>
+                            <td><label>{props.userdata.hiredDate}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faStar} /></td>
+                            <td><label>{props.userdata.rating}</label></td>
+                        </tr>
+                        <tr>
+                            <td><FontAwesomeIcon icon={faUsers} /></td>
+                            <td><label>Team {props.userdata.team}</label></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
 		</Modal.Body>
